6520086aea982bce7f8dc595fe60e4cc76b18496
me-ran <75988843+0xh34rtl3ss@users.noreply.github.com>
2022-12-16 23:56:25 +0800

    add prayer times in json

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, jsonify,json, request
+from flask import Flask, render_template, jsonify
 from algo import generateRand
+from flask_cors import CORS
 import time
+import json
+import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -10,7 +14,25 @@ def home():
 
 @app.route('/api')
 def result_json():
-        data = {'num': generateRand()}
+
+        response = requests.get('https://solat.my/api/daily/sgr01')
+
+        if response.status_code == 200:
+        # success
+                data1 = response.json()
+                hijri = data1['prayerTime'][0]['hijri']
+                fajr = data1['prayerTime'][0]['fajr']
+                zuhr = data1['prayerTime'][0]['dhuhr']
+                asr = data1['prayerTime'][0]['asr']
+                maghrib = data1['prayerTime'][0]['maghrib']
+                isya = data1['prayerTime'][0]['isha']
+        # do something with the data here
+        else:
+        # error
+                print(f'Error: {response.status_code}')
+        
+        data = {'num': generateRand(),'hijri': hijri, 'fajr':fajr, 'zuhr':zuhr, 'asr':asr, 'maghrib':maghrib, 'isya':isya}
+        print(data)
         return jsonify(data)        
 
 
